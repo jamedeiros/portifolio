d56004a73bbf77d9eeaf18865ca9dff5b1c003e6
José Augusto <jamedeiros@gmail.com>
2022-04-06 01:46:24 -0300

    Adicionando interface

--- sorting/app.py
+++ sorting/app.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import random
 import threading
 import time
@@ -5,41 +6,67 @@ import algorithm
 
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.config import Config
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 
-
-WINDOWS_WIDTH = 500
+WINDOWS_WIDTH = 600
 WINDOWS_HEIGHT = 300
 SIZE = 50
 TIMEOUT = 0.01
 STEPS = 10
 
 
+KNOWN_ALGORITHIMS = {
+    "InsertionSort": algorithm.InsertionSort,
+    "SelectionSort": algorithm.SelectionSort,
+    "BubbleSort": algorithm.BubbleSort,
+    "CombSort": algorithm.CombSort,
+    "BogoSort": algorithm.BogoSort,
+}
+
 class SortingWidet(Widget):
     
-    def __init__(self, data, algorithm, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.data = data
-        self.data_size = len(data)
-        self.bar_size = WINDOWS_WIDTH // len(self.data)
 
         self.highlight = list()
-
-        self.algorithm = algorithm(data=self.data, display=self)
         self.is_running = True
+        self.algorithm_thread = None
+        self.delay = TIMEOUT
+
+    def set_data(self, data_size):
+        self.data = [random.randint(1, WINDOWS_HEIGHT + 1) for _ in  range(data_size)]
+        self.data_size = data_size
+        self.bar_size = int((WINDOWS_WIDTH*0.75) / self.data_size)
+    
+    def set_delay(self, delay):
+        self.delay = delay
+    
+    def set_algorithm(self, algorithm):
+        self.algorithm = algorithm(data=self.data, display=self)
+
+    def start(self):
+        if self.algorithm_thread:
+            self.is_running = False
+            self.algorithm_thread.join()
 
         self.algorithm_thread = threading.Thread(
             target=self.algorithm.process, args=(lambda: self.is_running,)
         )
+        self.is_running = True
         self.algorithm_thread.start()
-        
+
     def on_request_close(self, *args, **kwargs):
-        self.is_running = False
-        self.algorithm_thread.join()
+        if self.algorithm_thread:
+            self.is_running = False
+            self.algorithm_thread.join()
 
     def add_highlight(self, element):
         if isinstance(element, list):
@@ -49,7 +76,7 @@ class SortingWidet(Widget):
 
     def draw(self, *args, **kwargs):
         Clock.schedule_once(self.update_data, 0)
-        time.sleep(TIMEOUT)
+        time.sleep(self.delay)
 
     def update_data(self, *args):
         self.update()
@@ -65,31 +92,73 @@ class SortingWidet(Widget):
         self.highlight.clear()
 
 
+class Form(GridLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = 'vertical' 
+        self.cols = 2
+
+        self.add_widget(Label(text="Elements"))
+        self.elements = TextInput(text="100", multiline=False)
+        self.add_widget(self.elements)
+
+        self.add_widget(Label(text="Delay"))
+        self.delay = TextInput(text=str(TIMEOUT), multiline=False)
+        self.add_widget(self.delay)
+
+        dropdown = DropDown()
+
+        for name in KNOWN_ALGORITHIMS:
+            btn = Button(
+                text=name,
+                size_hint_y=None,
+                height=44
+            )
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+        
+        self.algorithmBtn = Button(text="Select")
+        self.algorithmBtn.bind(on_release=dropdown.open)
+
+        dropdown.bind(on_select=lambda instance, x: setattr(self.algorithmBtn, 'text', x))
+
+        self.add_widget(Label(text="Algorithms"))
+        self.add_widget(self.algorithmBtn)
+
 class Application(App):
 
-    def __init__(self, algorithm, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.algorithm = algorithm
-        self.data = [random.randint(1, WINDOWS_HEIGHT + 1) for _ in  range(SIZE)]
-
     def build(self):
-        myDisplay = SortingWidet(
-            data=self.data, 
-            algorithm=self.algorithm
-        )
-        Window.bind(on_request_close=myDisplay.on_request_close)
-        return myDisplay
+        self.window = GridLayout(cols=2)
+        self.main_widget = SortingWidet()
+        self.form = Form()
+
+        btn1 = Button(text='Run ...')
+        btn1.bind(on_release = self.execute)
+
+        formGrid = GridLayout(cols=1, size_hint=(0.5, 1))
+        formGrid.add_widget(self.form)
+        formGrid.add_widget(btn1)
+
+        side = GridLayout(cols=1)
+        side.add_widget(self.main_widget)
+        
+        self.window.add_widget(side)
+        self.window.add_widget(formGrid)
+
+        Window.bind(on_request_close=self.main_widget.on_request_close)
+
+        return self.window
+
+    def execute(self, *args, **kwargs):
+        self.main_widget.set_data(int(self.form.elements.text))
+        self.main_widget.set_delay(float(self.form.delay.text))
+        self.main_widget.set_algorithm(KNOWN_ALGORITHIMS[self.form.algorithmBtn.text])
+        self.main_widget.start()
 
 def main():
-    Config.set('graphics', 'width', WINDOWS_WIDTH)
-    Config.set('graphics', 'height', WINDOWS_HEIGHT)
-    Config.write()    
-
-    # Application(algorithm=algorithm.InsertionSort).run()
-    # Application(algorithm=algorithm.SelectionSort).run()
-    Application(algorithm=algorithm.BubbleSort).run()
-    # Application(algorithm=algorithm.CombSort).run()
-    # Application(algorithm=algorithm.BogoSort).run()
+    Window.size = (WINDOWS_WIDTH, WINDOWS_HEIGHT)
+    Application().run()
     
 
 
